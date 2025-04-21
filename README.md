d32a571e0ea39a3e801abaf3f308a5c0a4609bfa
Juan Simancas <dev.juansimancas@proton.me>
2025-04-21 17:08:33 -0500

    update controller logic

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ bun run dev
 
 
 TODO:
-- Update controllers to being handled with Elysia
+- ~~Update controllers to being handled with Elysia~~
+- Manage id with uuid
+- Use drizzle typebox to manage elysia's route validation
+- Error handling
 - Add testing modules
 - Add pipelines
--- src/routes/BlogRoutes.ts
+++ src/routes/BlogRoutes.ts
@@ -1,35 +1,121 @@
 import { Elysia, t } from 'elysia';
 import { blogController } from '../controllers/BlogController';
+import { BlogRepository } from '../repositories/BlogRepository';
+import { BlogService } from '../services/BlogService';
+
+const blogRepo = new BlogRepository();
+const blogService = new BlogService(blogRepo);
 
 export const blogRoutes = new Elysia({ prefix: '/blogs', tags: ['Blog'] })
-    .get('/', blogController.getAll, {
+    .get('/', async () => {
+        const blogs = await blogService.getAll();
+
+        return blogs
+    }, {
         detail: {
             summary: 'Get all blogs',
-        },
+            responses: {
+                "200": {
+                    description: "Get all blogs in the resource",
+                    content: { "application/json": {} }
+                },
+            },
+        }
     })
-    .get('/:id', blogController.getOne, {
+    .get('/:id', async ({ params, set }) => {
+        const blog = await blogService.getOne(Number(params.id));
+
+        if (!blog) {
+            set.status = 404;
+            return { error: "Not found" };
+        }
+
+        return blog;
+    }, {
         params: t.Object({ id: t.String() }),
         detail: {
             summary: 'Get a blog by ID',
+            responses: {
+                "200": {
+                    description: "Get a single blog in the resource",
+                    content: { "application/json": {} }
+                },
+                "404": {
+                    description: "Blog not found",
+                    content: { "application/json": {} }
+                },
+            },
         },
     })
-    .post('/', blogController.create, {
-        body: blogController.schema.create,
+    .post('/', async ({ body, set }) => {
+        const blog = await blogService.create(body);
+
+        set.status = 201
+        return blog;
+    }, {
+        body: t.Object({
+            title: t.String(),
+            description: t.Optional(t.String()),
+            slug: t.String(),
+            creator: t.String(),
+        }),
         detail: {
             summary: 'Create a blog',
-        },
+            responses: {
+                "201": {
+                    description: "Created a blog resource",
+                    content: { "application/json": {} }
+                },
+            },
+        }
     })
-    .delete('/:id', blogController.delete, {
+    .delete('/:id', async ({ params, set }) => {
+        await blogService.delete(Number(params.id));
+
+        set.status = 204;
+        return null;
+    }, {
         params: t.Object({ id: t.String() }),
         detail: {
             summary: 'Delete a blog',
+            responses: {
+                "204": {
+                    description: "Deleted a blog resource",
+                    content: { "application/json": {} }
+                },
+            }
         },
+
     })
-    .put('/:id', blogController.update, {
+    .put('/:id', async ({ params, body, set }) => {
+        const blogToUpdate = await blogService.getOne(Number(params.id));
+
+        if (!blogToUpdate) {
+            set.status = 404;
+            return { error: "blog not found" };
+        }
+
+        const updatedBlog = await blogService.update(Number(params.id), body);
+
+        set.status = 204
+        return updatedBlog;
+    }, {
         params: t.Object({ id: t.String() }),
-        body: blogController.schema.update,
+        body:
+            t.Object({
+                title: t.Optional(t.String()),
+                description: t.Optional(t.String()),
+                slug: t.Optional(t.String()),
+                creator: t.Optional(t.String()),
+            }),
         detail: {
             summary: 'Update a blog',
+            responses: {
+                "204": {
+                    description: "Update a blog resource",
+                    content: { "application/json": {} }
+                },
+            }
         }
     })
     ;
