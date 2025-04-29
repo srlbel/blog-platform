4eed1ebf206a7697b29f53350d8f50c6f0fdac74
Juan Simancas <dev.juansimancas@proton.me>
2025-04-29 11:10:44 -0500

    test pipeline with new drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,17 @@ import { config as dotenvConfig } from 'dotenv';
 
 dotenvConfig({ path: `.env.${process.env.NODE_ENV || 'dev'}` });
 
+let url = ':memory:';
+
+if (process.env.NODE_ENV != 'test') {
+  url = process.env.DB_FILE_NAME!;
+}
+
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
   dialect: 'sqlite',
   dbCredentials: {
-    url: process.env.DB_FILE_NAME!,
+    url: url,
   },
 });
