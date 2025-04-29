8e3d4fe4338c19aceb1c512e23956dfbf5f5d1d3
Juan Simancas <dev.juansimancas@proton.me>
2025-04-29 10:17:46 -0500

    update build command

--- build_prod.sh
+++ build_prod.sh
@@ -2,14 +2,12 @@
 
 bun install
 
-# Check if a file exists, if it exists, run a command
 FILE1="./test.db"
 if [ ! -f "$FILE1" ]; then
     echo "File $FILE1 exists. Creating it."
     Bun run db:push:test
 fi
 
-# Run a command and wait for it to finish
 echo "Running tests..."
 bun run test
 if [ $? -ne 0 ]; then
@@ -17,12 +15,10 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Step 3: Check if a file exists, if it exists, run a command
 FILE2="./prod.db"
 if [ ! -f "$FILE2" ]; then
     echo "File $FILE2 exists. Running command..."
     bun run db:push:prod
 fi
 
-# Replace with the actual long-running command
-bun run prod
+bun run build
