39ad86b8bd16e48003ccb91b1d17578b7b8c9d79
Juan Simancas <dev.juansimancas@proton.me>
2025-05-11 12:21:08 -0500

    change pipeline and report generation

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'cypress/reports',
-    overwrite: false,
+    overwrite: true,
     html: true,
     json: true,
   },
